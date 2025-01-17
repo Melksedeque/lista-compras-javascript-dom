c6464a9f05f4279205175111f68a7ed7fc06dff3
Melksedeque <freelancer@melksedeque.com.br>
2025-01-17 20:34:20 -0300

    Adição de data com new Date; formatação de data e hora com toLocaleDateString e toLocaleTimeString

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const inputItem = document.getElementById("input-item");
 const botaoAdicionar = document.getElementById("adicionar-item");
 const listaCompras = document.getElementById("lista-de-compras");
+const diaDaSemana = new Date().toLocaleDateString("pt-BR", { weekday: "long" });
+const data = new Date().toLocaleDateString("pt-BR");
+const hora = new Date().toLocaleTimeString("pt-BR", {
+  hour: "numeric",
+  minute: "numeric",
+});
+const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
 let contador = 1;
 
 botaoAdicionar.addEventListener("click", (e) => {
@@ -16,15 +23,28 @@ botaoAdicionar.addEventListener("click", (e) => {
   const containerItemDaLista = document.createElement("div");
   const inputCheckbox = document.createElement("input");
   const nomeItem = document.createElement("p");
+  const itemData = document.createElement("p");
 
   containerItemDaLista.classList.add("lista-item-container");
   inputCheckbox.type = "checkbox";
   inputCheckbox.id = "checkbox-" + contador++;
   nomeItem.innerText = inputItem.value;
+  itemData.classList.add("texto-data");
+  itemData.innerText = dataCompleta;
+
+  inputCheckbox.addEventListener("click", () => {
+    if (inputCheckbox.checked) {
+      nomeItem.style.textDecoration = "line-through";
+    } else {
+      nomeItem.style.textDecoration = "none";
+    }
+  });
 
   containerItemDaLista.appendChild(inputCheckbox);
   containerItemDaLista.appendChild(nomeItem);
   itemDaLista.appendChild(containerItemDaLista);
+  itemDaLista.appendChild(itemData);
+
   listaCompras.appendChild(itemDaLista);
 
   console.log(containerItemDaLista);
