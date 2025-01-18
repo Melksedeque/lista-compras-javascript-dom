04ecd92a6db48e897c49cb9476c5df6fd6165d50
Melksedeque <freelancer@melksedeque.com.br>
2025-01-17 21:37:06 -0300

    Remoção de funcionalidade da Data Completa para um arquivo separado

--- script.js
+++ script.js
@@ -3,17 +3,10 @@ import { verificarListaVazia } from "./js/verificarListaVazia.js";
 
 const botaoAdicionar = document.getElementById("adicionar-item");
 const listaCompras = document.getElementById("lista-de-compras");
-const diaDaSemana = new Date().toLocaleDateString("pt-BR", { weekday: "long" });
-const data = new Date().toLocaleDateString("pt-BR");
-const hora = new Date().toLocaleTimeString("pt-BR", {
-  hour: "numeric",
-  minute: "numeric",
-});
-const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
 
 botaoAdicionar.addEventListener("click", (e) => {
   e.preventDefault();
-  const itemDaLista = criarItemDaLista(dataCompleta);
+  const itemDaLista = criarItemDaLista();
   listaCompras.appendChild(itemDaLista);
   verificarListaVazia(listaCompras);
 });
